a4ce5e4d71366d02e05b7ab0bab3c5f3e2ad4652
xuexiangjys <xuexiangjys@163.com>
2018-11-05 09:20:29 +0800

    为默认下载者增加服务解绑的功能，及时释放资源

--- xupdate-lib/src/main/java/com/xuexiang/xupdate/proxy/impl/DefaultUpdateDownloader.java
+++ xupdate-lib/src/main/java/com/xuexiang/xupdate/proxy/impl/DefaultUpdateDownloader.java
@@ -22,10 +22,9 @@ import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import com.xuexiang.xupdate.entity.DownloadEntity;
+import com.xuexiang.xupdate.XUpdate;
 import com.xuexiang.xupdate.entity.UpdateEntity;
 import com.xuexiang.xupdate.proxy.IUpdateDownloader;
-import com.xuexiang.xupdate.proxy.IUpdateHttpService;
 import com.xuexiang.xupdate.service.DownloadService;
 import com.xuexiang.xupdate.service.OnFileDownloadListener;
 
@@ -39,28 +38,51 @@ public class DefaultUpdateDownloader implements IUpdateDownloader {
 
     private DownloadService.DownloadBinder mDownloadBinder;
 
+    /**
+     * 服务绑定连接
+     */
+    private ServiceConnection mServiceConnection;
+
+    /**
+     * 是否已绑定下载服务
+     */
+    private boolean mIsBound;
+
+    @Override
+    public void startDownload(final @NonNull UpdateEntity updateEntity, final @Nullable OnFileDownloadListener downloadListener) {
+        DownloadService.bindService(mServiceConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                mIsBound = true;
+                startDownload((DownloadService.DownloadBinder) service, updateEntity, downloadListener);
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                mIsBound = false;
+            }
+        });
+    }
+
+    /**
+     * 开始下载
+     * @param binder
+     * @param updateEntity
+     * @param downloadListener
+     */
     private void startDownload(DownloadService.DownloadBinder binder, @NonNull UpdateEntity updateEntity, @Nullable OnFileDownloadListener downloadListener) {
         mDownloadBinder = binder;
         mDownloadBinder.start(updateEntity, downloadListener);
     }
 
     @Override
-    public void startDownload(final @NonNull UpdateEntity updateEntity, final @Nullable OnFileDownloadListener downloadListener) {
-        DownloadService.bindService(new ServiceConnection() {
-            @Override
-            public void onServiceConnected(ComponentName name, IBinder service) {
-                startDownload((DownloadService.DownloadBinder) service, updateEntity, downloadListener);
-            }
-            @Override
-            public void onServiceDisconnected(ComponentName name) {
-            }
-        });
-    }
-
-    @Override
     public void cancelDownload() {
         if (mDownloadBinder != null) {
             mDownloadBinder.stop("取消下载");
+        }
+        if (mIsBound && mServiceConnection != null) {
+            XUpdate.getContext().unbindService(mServiceConnection);
+            mIsBound = false;
         }
     }
 }
