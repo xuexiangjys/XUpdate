033fa93e4c489c7ae5bf4fd9d3b2abdc988bba13
xuexiangjys <xuexiangjys@163.com>
2018-11-18 00:32:25 +0800

    调整debug的位置

--- app/src/main/java/com/xuexiang/xupdatedemo/App.java
+++ app/src/main/java/com/xuexiang/xupdatedemo/App.java
@@ -77,12 +77,12 @@ public class App extends Application {
 
     private void initUpdate() {
         XUpdate.get()
+                .debug(true)
                 .isWifiOnly(true)     //默认设置只在wifi下检查版本更新
                 .isGet(true)          //默认设置使用get请求检查版本
                 .isAutoMode(false)    //默认设置非自动模式，可根据具体使用配置
                 .param("versionCode", UpdateUtils.getVersionCode(this)) //设置默认公共请求参数
                 .param("appKey", getPackageName())
-                .debug(true)
                 .setOnUpdateFailureListener(new OnUpdateFailureListener() { //设置版本更新出错的监听
                     @Override
                     public void onFailure(UpdateError error) {
