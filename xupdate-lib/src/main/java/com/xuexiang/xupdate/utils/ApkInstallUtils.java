f4ca54acb8472950fe7f27c19ca85c8d10c12549
xuexiangjys <xuexiangjys@163.com>
2019-02-21 22:51:45 +0800

    修改安装工具类的方法，修复可能存在的bug

--- xupdate-lib/src/main/java/com/xuexiang/xupdate/utils/ApkInstallUtils.java
+++ xupdate-lib/src/main/java/com/xuexiang/xupdate/utils/ApkInstallUtils.java
@@ -239,7 +239,7 @@ public final class ApkInstallUtils {
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 //区别于 FLAG_GRANT_READ_URI_PERMISSION 跟 FLAG_GRANT_WRITE_URI_PERMISSION， URI权限会持久存在即使重启，直到明确的用 revokeUriPermission(Uri, int) 撤销。 这个flag只提供可能持久授权。但是接收的应用必须调用ContentResolver的takePersistableUriPermission(Uri, int)方法实现
-                intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
                 Uri fileUri = FileProvider.getUriForFile(context, context.getApplicationContext().getPackageName() + ".updateFileProvider", appFile);
                 intent.setDataAndType(fileUri, "application/vnd.android.package-archive");
             } else {
