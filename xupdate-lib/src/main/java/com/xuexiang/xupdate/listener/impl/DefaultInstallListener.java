66514b4aa9da54472d1eb904d4e0130c550b4587
xuexiangjys <xuexiangjys@163.com>
2019-09-04 11:31:42 +0800

    优化默认安装监听的代码

--- xupdate-lib/src/main/java/com/xuexiang/xupdate/listener/impl/DefaultInstallListener.java
+++ xupdate-lib/src/main/java/com/xuexiang/xupdate/listener/impl/DefaultInstallListener.java
@@ -30,7 +30,7 @@ import java.io.IOException;
 import static com.xuexiang.xupdate.entity.UpdateError.ERROR.INSTALL_FAILED;
 
 /**
- * 默认的apk安装监听
+ * 默认的apk安装监听【自定义安装监听可继承该类，并重写相应的方法】
  *
  * @author xuexiang
  * @since 2018/7/1 下午11:58
@@ -39,13 +39,41 @@ public class DefaultInstallListener implements OnInstallListener {
 
     @Override
     public boolean onInstallApk(@NonNull Context context, @NonNull File apkFile, @NonNull DownloadEntity downloadEntity) {
+        if (checkApkFile(downloadEntity, apkFile)) {
+            return installApkFile(context, apkFile);
+        } else {
+            _XUpdate.onUpdateError(INSTALL_FAILED, "apk文件校验不通过！");
+            return false;
+        }
+    }
+
+    /**
+     * 检验apk文件的有效性（默认是使用MD5进行校验,可重写该方法）
+     *
+     * @param downloadEntity
+     * @param apkFile
+     * @return
+     */
+    protected boolean checkApkFile(DownloadEntity downloadEntity, @NonNull File apkFile) {
+        return downloadEntity != null && downloadEntity.isApkFileValid(apkFile);
+    }
+
+    /**
+     * 安装apk文件【此处可自定义apk的安装方法,可重写该方法】
+     *
+     * @param context
+     * @param apkFile
+     * @return
+     */
+    protected boolean installApkFile(Context context, File apkFile) {
         try {
-            return downloadEntity.isApkFileValid(apkFile) && ApkInstallUtils.install(context, apkFile);
+            return ApkInstallUtils.install(context, apkFile);
         } catch (IOException e) {
             _XUpdate.onUpdateError(INSTALL_FAILED, "获取apk的路径出错！");
         }
         return false;
     }
+
 
     @Override
     public void onInstallApkSuccess() {
